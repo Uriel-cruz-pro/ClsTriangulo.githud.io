7fbf179c22ac85a9dae84671a3d944474b343be0
Uriel-cruz-pro <ucruz4095@gmail.com>
2025-05-21 19:54:46 -0600

    Add files via upload

---
+++ Figura.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClsTriangulo
+{
+    class ClsTriangulo
+    {
+        // Atributos privados
+        private float LadoA;
+        private float LadoB;
+        private float LadoC;
+
+        // Métodos para acceder a los atributos
+        public float getLadoA()
+        {
+            return LadoA;
+        }
+
+        public void setLadoA(float valor)
+        {
+            LadoA = valor;
+        }
+
+        public float getLadoB()
+        {
+            return LadoB;
+        }
+
+        public void setLadoB(float valor)
+        {
+            LadoB = valor;
+        }
+
+        public float getLadoC()
+        {
+            return LadoC;
+        }
+
+        public void setLadoC(float valor)
+        {
+            LadoC = valor;
+        }
+
+        // Constructor por defecto
+        public ClsTriangulo()
+        {
+            LadoA = 0;
+            LadoB = 0;
+            LadoC = 0;
+        }
+
+        // Método para calcular el perímetro
+        public float perimetro()
+        {
+            return LadoA + LadoB + LadoC;
+        }
+
+        // Método para calcular el área usando la fórmula de Herón
+        public double area()
+        {
+            double s = perimetro() / 2;
+            return Math.Sqrt(s * (s - LadoA) * (s - LadoB) * (s - LadoC));
+        }
+
+        // Método para determinar el tipo de triángulo
+        public string definirTipoTriangulo()
+        {
+            if (LadoA + LadoB <= LadoC || LadoA + LadoC <= LadoB || LadoB + LadoC <= LadoA)
+            {
+                return "Los valores ingresados no forman un triángulo válido.";
+            }
+            if (LadoA == LadoB && LadoB == LadoC)
+            {
+                return "Equilátero";
+            }
+            else if (LadoA == LadoB || LadoB == LadoC || LadoA == LadoC)
+            {
+                return "Isósceles";
+            }
+            else
+            {
+                return "Escaleno";
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ClsTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsTriangulo triangulo = new ClsTriangulo();
+
+            // Pedir valores al usuario
+            Console.WriteLine("Ingrese el primer lado del triángulo:");
+            triangulo.setLadoA(Convert.ToSingle(Console.ReadLine()));
+
+            Console.WriteLine("Ingrese el segundo lado del triángulo:");
+            triangulo.setLadoB(Convert.ToSingle(Console.ReadLine()));
+
+            Console.WriteLine("Ingrese el tercer lado del triángulo:");
+            triangulo.setLadoC(Convert.ToSingle(Console.ReadLine()));
+
+            // Mostrar los resultados
+            Console.WriteLine($"\nPerímetro: {triangulo.perimetro()}");
+            Console.WriteLine($"Área: {triangulo.area()}");
+            Console.WriteLine($"Tipo de Triángulo: {triangulo.definirTipoTriangulo()}");
+            Console.ReadKey();
+        }
+    }
+}
+
